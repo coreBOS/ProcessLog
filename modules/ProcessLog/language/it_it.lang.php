11e1b52df85c38add3ffd3a5238ad83ee2dfe317
Joe Bordes <joe@tsolucio.com>
2019-11-23 19:39:19 +0100

    Process Log module

---
+++ modules/ProcessLog/language/it_it.lang.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'ProcessLog' => 'Log di Processi',
+	'SINGLE_ProcessLog' => 'Log di Processo',
+	'ProcessLog ID' => 'ID Log Processi',
+
+	'LBL_CUSTOM_INFORMATION' => 'Informazione Personalizzata',
+	'LBL_ProcessLog_INFORMATION' => 'Informazione Log',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descrizione',
+	'processlog_no' => 'Nº Log',
+	'ProcessLog Name' => 'Nombre Log',
+	'Previous Substatus' => 'Estado Anterior',
+	'Next Substatus' => 'Estado Siguiente',
+	'Date Time' => 'Tiempo',
+);
+?>
